d437a69f4eca36e5dc22f9daa5b87e3368bbef5d
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-02-15 15:32:33 +0000

    small tweaks

--- models/seed-models.js
+++ models/seed-models.js
@@ -11,11 +11,8 @@ exports.fetchTopics = () => {
 exports.fetchArticleById = (article_id) => {
     return db
     .query(`
-    SELECT articles.*, COUNT(comments.article_id) AS comment_count
-    FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    WHERE articles.article_id = $1
-    GROUP BY articles.article_id;`,
+    SELECT * FROM articles
+    WHERE article_id = $1;`,
     [article_id])
     .then(({rows}) => {
         const article = rows[0]

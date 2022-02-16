e3d0ad29b25c1da89946cb61df9ada2c7170007a
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-02-16 12:20:17 +0000

    #10 added column comment_count into Get Articles

--- query.sql
+++ query.sql
@@ -1,3 +1,7 @@
 \c nc_news_test
 
-SELECT * FROM articles WHERE article_id = 6
+SELECT articles.*, COUNT(comments.article_id) AS comment_count 
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;

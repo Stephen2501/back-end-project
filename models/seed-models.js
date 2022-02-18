252b6d0d366469c7d4f705ecfe2619401b6196de
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-02-18 09:10:23 +0000

    removed console.log

--- models/seed-models.js
+++ models/seed-models.js
@@ -158,7 +158,6 @@ exports.checkTopicExists = (topic) => {
         RETURNING *;
     `, [comment_id])
     .then((result) => {
-      console.log(result)
       if(result.rows.length === 0) {
         return Promise.reject({
             status: 404,

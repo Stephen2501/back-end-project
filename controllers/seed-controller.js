d09a1fe2eff3f376172750d2b64b3dcf8ba48b3d
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-04-05 09:33:49 +0100

    Grouped controllers

--- controllers/seed-controller.js
+++ controllers/seed-controller.js
@@ -13,44 +13,27 @@ const {
 } = require("../models/seed-models");
 
 
-
-exports.getTopics = (req, res, next) => {
-  fetchTopics()
-    .then((topics) => {
-      res.status(200).send({ topics });
-    })
-    .catch((err) => {
-      next(err);
-    });
-};
-
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticleById(article_id)
-    .then((article) => {
-      res.status(200).send({ article });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  .then((article) => {
+    res.status(200).send({ article });
+  })
+  .catch((err) => {
+    next(err);
+  });
 };
 
 exports.patchArticle = (req, res, next) => {
   const articleId = req.params.article_id;
-
-    updateArticle(articleId, req.body.votes).then((article) => {
-      res.status(200).send({ article });
-    })
-    .catch((err) => {
-        next(err)
-    })
-  } 
-
-exports.getUsers = (req, res, next) => {
-  fetchUsers().then((users) => {
-    res.status(200).send({ users });
-  });
-};
+  
+  updateArticle(articleId, req.body.votes).then((article) => {
+    res.status(200).send({ article });
+  })
+  .catch((err) => {
+    next(err)
+  })
+} 
 
 exports.getArticles = (req, res, next) => {
   const { sort_by, order_by, topic } = req.query;
@@ -58,8 +41,23 @@ exports.getArticles = (req, res, next) => {
     fetchArticles(sort_by, order_by, topic),
     checkTopicExists(topic),
   ])
-    .then(([articles]) => {
-      res.status(200).send({ articles: articles });
+  .then(([articles]) => {
+    res.status(200).send({ articles: articles });
+  })
+  .catch((err) => {
+    next(err);
+  });
+};
+
+exports.getUsers = (req, res, next) => {
+  fetchUsers().then((users) => {
+    res.status(200).send({ users });
+  });
+};
+exports.getTopics = (req, res, next) => {
+  fetchTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
     })
     .catch((err) => {
       next(err);
